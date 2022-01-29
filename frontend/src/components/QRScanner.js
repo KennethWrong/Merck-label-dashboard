b9dcd58782c82a2fb44145847e1e67f0f15a65b8
Chintan Sawla <59383889+sawlachintan@users.noreply.github.com>
2022-01-29 18:40:17 -0500

    refactor QRScanner

--- frontend/src/components/QRScanner.js
+++ frontend/src/components/QRScanner.js
@@ -1,112 +1,124 @@
-
 import { Box } from "@mui/system";
 import axios from "axios";
 import React, { useEffect } from "react";
-import {useState} from "react";
-import QRScan from 'react-qr-reader';
+import { useState } from "react";
+import QRScan from "react-qr-reader";
 import BasicTable from "./ScanTable";
-import { Alert } from '@mui/material';
+import { Alert } from "@mui/material";
 
-function QRScanner () {
-    const [qrScan, setQrScan] = useState('');
-    const [errorMessage, setErrorMessage] = useState(false)
-    const [successMessage, setSuccessMessage] = useState(false)
+function QRScanner() {
+  const [qrScan, setQrScan] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(false);
 
-    const [allValues, setAllValues] = useState({
-        qr_code_key: '',
-        PC: '',
-        sampleID: '',
-        batchID: '',
-        date: ''
-     });
+  const [allValues, setAllValues] = useState({
+    qr_code_key: "",
+    PC: "",
+    sampleID: "",
+    batchID: "",
+    date: "",
+  });
 
-
-    const handleScan = data => {
-        if (data) {
-            console.log(data)
-            let responseJSON = modifyToJSON(data)
-            setQrScan(responseJSON['qr_code_key'])
-        }
+  const handleScan = (data) => {
+    if (data) {
+      console.log(data);
+      let responseJSON = modifyToJSON(data);
+      setQrScan(responseJSON["qr_code_key"]);
     }
-    const handleError = e => {
-        console.error(e);
-    }
+  };
+  const handleError = (e) => {
+    console.error(e);
+  };
 
-    useEffect(()=>{
-        if(qrScan){
-            async function sendInfoToFlask(){
-                let obj = {
-                    'qr_code_key':qrScan
-                }
-                try{
-                    //post request to our backend
-                    let res,mes = await axios.post('http://localhost:5000/scan/qr_code',obj)
-                    let responseJSON = res.data
-                    if (mes == "fail") {
-                        setErrorMessage(true)
-                        setTimeout(() => {
-                            setErrorMessage(false)
-                        },1500)
-                    } else {
-                        setAllValues({
-                            qr_code_key : responseJSON.qr_code_key,
-                            PC : responseJSON.protein_concentration,
-                            sampleID : responseJSON.sample_id,
-                            batchID : responseJSON.batch_id,
-                            date : responseJSON.date_entered
-                        })
-                        setSuccessMessage(true)
-                        setTimeout(() => {
-                            setSuccessMessage(false)
-                        },1500)
-                     }
-                }
-                catch(e){
-                    console.log(e)
-                    setErrorMessage(true)
-                    setTimeout(() => {
-                      setErrorMessage(false)
-                    },1500)
-                }
-            }
-            sendInfoToFlask()
+  useEffect(() => {
+    if (qrScan) {
+      async function sendInfoToFlask() {
+        let obj = {
+          qr_code_key: qrScan,
+        };
+        try {
+          //post request to our backend
+          let res,
+            mes = await axios.post("http://localhost:5000/scan/qr_code", obj);
+          let responseJSON = res.data;
+          if (mes === "fail") {
+            setErrorMessage(true);
+            setTimeout(() => {
+              setErrorMessage(false);
+            }, 1500);
+          } else {
+            setAllValues({
+              qr_code_key: responseJSON.qr_code_key,
+              PC: responseJSON.protein_concentration,
+              sampleID: responseJSON.sample_id,
+              batchID: responseJSON.batch_id,
+              date: responseJSON.date_entered,
+            });
+            setSuccessMessage(true);
+            setTimeout(() => {
+              setSuccessMessage(false);
+            }, 1500);
+          }
+        } catch (e) {
+          console.log(e);
+          setErrorMessage(true);
+          setTimeout(() => {
+            setErrorMessage(false);
+          }, 1500);
         }
-    },[qrScan])
+      }
+      sendInfoToFlask();
+    }
+  }, [qrScan]);
 
-    return (
-        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Qr Scanner</h1>
+      <div>
+        <QRScan
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ height: 240, width: 320, borderRadius: 5 }}
+        />
+      </div>
+      <Box sx={{ mt: 12 }}>
+        <BasicTable info={allValues} />
+      </Box>
 
-        <h1>Qr Scanner</h1>
-        <div>
-            <QRScan
-            delay = {300}
-            onError={handleError}
-            onScan={handleScan}
-            style={{height: 240, width: 320, borderRadius:5}}
-            />
-        </div>
-        <Box sx={{ mt: 12, }}>
-        <BasicTable info={allValues}/>
+      {successMessage ? (
+        <Box mt={5} mb={5}>
+          <Alert severity="success" style={{ fontSize: "40px" }}>
+            Successfully Scanned Item
+          </Alert>
         </Box>
-
-        {successMessage?
+      ) : (
+        ""
+      )}
+      {errorMessage ? (
         <Box mt={5} mb={5}>
-            <Alert severity="success" style={{ fontSize: '40px' }}>Successfully Scanned Item</Alert>
-        </Box>:''
-        }
-        {errorMessage?
-        <Box mt={5} mb={5}>
-            <Alert severity="error" style={{ fontSize: '40px' }}>Scan was unsuccessful, try again please</Alert>
-        </Box>:''
-        }
-        </div>
-    )
+          <Alert severity="error" style={{ fontSize: "40px" }}>
+            Scan was unsuccessful, try again please
+          </Alert>
+        </Box>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 const modifyToJSON = (message) => {
-    message = message.replace(/'/g, '"')
-    let responseJSON = JSON.parse(message)
-    return responseJSON
-}
+  message = message.replace(/'/g, '"');
+  let responseJSON = JSON.parse(message);
+  return responseJSON;
+};
 
 export default QRScanner;
